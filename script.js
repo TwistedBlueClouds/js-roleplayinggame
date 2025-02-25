bc69a07203a58e14f70777492fc3894133c09ffe
TwistedBlueClouds <andrew.vergara@gmail.com>
2025-02-25 08:52:57 -0500

    updated javascript code to latest section of the freecodecamp steps im at. Added comments to help me understand what each section does.

--- script.js
+++ script.js
@@ -1,12 +1,185 @@
-let xp = 0;
-let health = 100;
-let gold = 50;
-let currentWeaponIndex = 0;
-let fighting;
-let monsterHealth;
-let inventory = ["stick"];
+// Set up the variables for the game.
+let xp = 0;  // experience points
+let health = 100; // player health
+let gold = 50; // player gold
+let currentWeaponIndex = 0; // index of the current weapon in the weapons array
+let fighting;   // index of the monster in the monsters array
+let monsterHealth; // monster health
+let inventory = ["stick"]; // player inventory
 
-// Using constants 
-const button1 = document.querySelector("#button1");
-const button2 = document.querySelector("#button2");
-const button3 = document.querySelector("#button3");
+/*
+This set upts the buttons and text for the game based on the HTML and CSS of the HTML file.
+*/
+const button1 = document.querySelector('#button1'); // button 1
+const button2 = document.querySelector("#button2"); // button 2
+const button3 = document.querySelector("#button3"); // button 3
+const text = document.querySelector("#text"); // This is the text that will be displayed to the player
+const xpText = document.querySelector("#xpText"); // This is the text that will display the player's experience points
+const healthText = document.querySelector("#healthText"); // This is the text that will display the player's health
+const goldText = document.querySelector("#goldText"); // This is the text that will display the player's gold
+const monsterStats = document.querySelector("#monsterStats"); // This is the div that will display the monster's stats
+const monsterName = document.querySelector("#monsterName"); // This is the text that will display the monster's name
+const monsterHealthText = document.querySelector("#monsterHealth"); // This is the text that will display the monster's health
+
+// Set up the weapons in the game
+const weapons = [
+  { name: 'stick', power: 5 },
+  { name: 'dagger', power: 30 },
+  { name: 'claw hammer', power: 50 },
+  { name: 'sword', power: 100 }
+];
+// Set up the monsters for the game
+const monsters = [
+  {
+    name: "slime",
+    level: 2,
+    health: 15
+  },
+  {
+    name: "fanged beast",
+    level: 8,
+    health: 60
+  },
+  {
+    name: "dragon",
+    level: 20,
+    health: 300
+  }
+]
+// Set up the locations for the game.
+const locations = [
+  {
+    name: "town square",
+    "button text": ["Go to store", "Go to cave", "Fight dragon"],
+    "button functions": [goStore, goCave, fightDragon],
+    text: "You are in the town square. You see a sign that says \"Store\"."
+  },
+  {
+    name: "store",
+    "button text": ["Buy 10 health (10 gold)", "Buy weapon (30 gold)", "Go to town square"],
+    "button functions": [buyHealth, buyWeapon, goTown],
+    text: "You enter the store."
+  },
+  {
+    name: "cave",
+    "button text": ["Fight slime", "Fight fanged beast", "Go to town square"],
+    "button functions": [fightSlime, fightBeast, goTown],
+    text: "You enter the cave. You see some monsters."
+  },
+  {
+    name: "fight",
+    "button text": ["Attack", "Dodge", "Run"],
+    "button functions": [attack, dodge, goTown],
+    text: "You are fighting a monster."
+  }
+];
+
+
+/*
+Start the functions and meat of the game.
+*/
+// set up the buttons in the begining of the game or town square. 
+button1.onclick = goStore;
+button2.onclick = goCave;
+button3.onclick = fightDragon;
+
+
+function update(location) {
+  button1.innerText = location["button text"][0];
+  button2.innerText = location["button text"][1];
+  button3.innerText = location["button text"][2];
+  button1.onclick = location["button functions"][0];
+  button2.onclick = location["button functions"][1];
+  button3.onclick = location["button functions"][2];
+  text.innerText = location.text;
+}
+
+function goTown() {
+  update(locations[0]);
+}
+
+function goStore() {
+  update(locations[1]);
+}
+
+function goCave() {
+  update(locations[2]);
+}
+
+function buyHealth() {
+  if (gold >= 10) {
+    gold -= 10;
+    health += 10;
+    goldText.innerText = gold;
+    healthText.innerText = health;
+  } else {
+    text.innerText = "You do not have enough gold to buy health.";
+  }
+}
+
+function buyWeapon() {
+  if (currentWeaponIndex < weapons.length - 1) {
+    if (gold >= 30) {
+      gold -= 30;
+      currentWeaponIndex++;
+      goldText.innerText = gold;
+      let newWeapon = weapons[currentWeaponIndex].name;
+      text.innerText = "You now have a " + newWeapon + ".";
+      inventory.push(newWeapon);
+      text.innerText += " In your inventory you have: " + inventory;
+    } else {
+      text.innerText = "You do not have enough gold to buy a weapon.";
+    }
+  } else {
+    text.innerText = "You already have the most powerful weapon!";
+    button2.innerText = "Sell weapon for 15 gold";
+    button2.onclick = sellWeapon;
+  }
+}
+
+function sellWeapon() {
+  if (inventory.length > 1) {
+    gold += 15;
+    goldText.innerText = gold;
+    let currentWeapon = inventory.shift();
+    text.innerText = "You sold a " + currentWeapon + ".";
+    text.innerText += " In your inventory you have: " + inventory;
+  } else {
+    text.innerText = "Don't sell your only weapon!";
+  }
+}
+
+function fightSlime() {
+  fighting = 0;
+  goFight();
+}
+
+function fightBeast() {
+  fighting = 1;
+  goFight();
+}
+
+function fightDragon() {
+  fighting = 2;
+  goFight();
+}
+
+function goFight() {
+  update(locations[3]);
+  monsterHealth = monsters[fighting].health;
+  monsterStats.style.display = "block";
+  monsterName.innerText = monsters[fighting].name;
+  monsterHealthText.innerText = monsterHealth;
+}
+
+function attack() {
+  text.innerText = "The " + monsters[fighting].name + " attacks.";
+  text.innerText += " You attack it with your " + weapons[currentWeaponIndex].name + ".";
+  health -= monsters[fighting].level;
+  monsterHealth -= weapons[currentWeaponIndex].power + Math.floor(Math.random() * xp) + 1;
+  
+}
+
+function dodge() {
+
+}
